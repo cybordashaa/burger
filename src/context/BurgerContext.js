eb45569db174759b648e48b68973991910a7b126
dashaa <cybordashaa@gmail.com>
2020-05-13 18:53:43 +0800

    add(): Context API болон useContext, useState hook хэрэглэн Burger компонентийг REDUX-аас hooks рүү шилжүүлцгээе!

--- src/context/BurgerContext.js
+++ src/context/BurgerContext.js
@@ -1,3 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default React.createContext('hello! context');
+const BurgerContext = React.createContext();
+
+export const BurgerStore = props => {
+    const [ingredients, setIngredients] = useState({
+        salad: 0,
+        cheese: 0,
+        bacon: 0,
+        meat: 0
+    });
+
+    const addIngredient = (orts) => {
+        console.log('add ...');
+
+    }
+
+    const removeIngredient = (orts) => {
+        console.log('remove ...');
+
+    };
+
+    return (
+        <BurgerContext.Provider value={ingredients, addIngredient, removeIngredient}>{props.children}</BurgerContext.Provider>
+    )
+
+};
+
+
+export default BurgerContext;
