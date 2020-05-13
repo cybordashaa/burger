968194f4f4ceecbc7aa320584176fd534b48c739
dashaa <cybordashaa@gmail.com>
2020-05-13 16:49:45 +0800

    add(): Javascript файлуудыг хэрэгтэй үед нь л ачаалах буюу React.lazy, Suspence ашиглан Lazy loading гүйцэтгэх

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,18 +1,32 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect, Suspense } from "react";
 import { Redirect } from 'react-router-dom';
 import css from "./style.module.css";
 import { connect } from 'react-redux';
 import Toolbar from "../../components/ToolBar";
-import BurgerPage from "../BurgerPage";
 import SideBar from "../../components/SideBar";
-import OrderPage from "../OrderPage";
 import { Route, Switch } from 'react-router-dom';
 import ShippingPage from "../ShippingPage";
 import Login from "../Login";
-import Signup from "../signUpPage";
 import Logout from "../../components/logout";
 import * as actions from '../../redux/actions/loginAction';
 import * as logout from '../../redux/actions/signupActions';
+
+
+// import BurgerPage from "../BurgerPage";
+// import OrderPage from "../OrderPage";
+// import Signup from "../signUpPage";
+
+
+const BurgerPage = React.lazy(() => {
+  return import("../BurgerPage");
+});
+const OrderPage = React.lazy(() => {
+  return import("../OrderPage");
+})
+const Signup = React.lazy(() => {
+  return import("../signUpPage");
+})
+
 
 const App = props => {
 
@@ -52,21 +66,23 @@ const App = props => {
       />
 
       <main className={css.Content}>
-        UserId : {props.userId}
-        {props.userId ? (<Fragment>
-          <Switch>
-            <Route path="/logout" component={Logout} />
-            <Route path="/orders" component={OrderPage} />
-            <Route path="/ship" component={ShippingPage} />
-            <Route
-              path='/' component={BurgerPage}
-            /></Switch>
-        </Fragment>) : (<Fragment><Switch>
-          <Route path="/signUp" component={Signup} />
-          <Route path="/login" component={Login} />
-          <Redirect to="/login" />
-          {/* <Route path="/" component={BurgerPage} /> */}
-        </Switch></Fragment>)}
+        <Suspense fallback={<div>Loading ....</div>}>
+          UserId : {props.userId}
+          {props.userId ? (<Fragment>
+            <Switch>
+              <Route path="/logout" component={Logout} />
+              <Route path="/orders" component={OrderPage} />
+              <Route path="/ship" component={ShippingPage} />
+              <Route
+                path='/' component={BurgerPage}
+              /></Switch>
+          </Fragment>) : (<Fragment><Switch>
+            <Route path="/signUp" component={Signup} />
+            <Route path="/login" component={Login} />
+            <Redirect to="/login" />
+            {/* <Route path="/" component={BurgerPage} /> */}
+          </Switch></Fragment>)}
+        </Suspense>
 
       </main>
     </div>
